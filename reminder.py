025566e452f26602b5b492c0f9b91cc36a6b2ae9
Mikhail Skvortsov <36133618+NeoPicos@users.noreply.github.com>
2020-04-04 22:07:01 +0300

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -7,7 +7,10 @@ def check_user_time():
     # Получить user id для напомининя
     user = User()
     id_list = user.get_user_id_for_remind()
-
+    
+    if len(id_list) == 0:
+        return
+    
     # Отправить пользователям сообщение-напоминание
     for id in id_list:
         remind(str(id))
