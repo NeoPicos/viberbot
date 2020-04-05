7cd0a348d949c771443fe2c6318e022d1f1e6e27
Mikhail Skvortsov <36133618+NeoPicos@users.noreply.github.com>
2020-04-05 21:37:34 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,13 +3,20 @@ from reminder import check_user_time
 import requests
 from settings import URL
 
-sched = BlockingScheduler()
-
+sched_remind = BlockingScheduler()
+sched_request = BlockingCheduler()
 
 # Напоминание
-@sched.scheduled_job('interval', minutes=1)
+@sched_remind.scheduled_job('interval', minutes=1)
 def do_reminder():
     check_user_time()
 
     
-sched.start()
+# Фиктивный запрос
+@sched_request.scheduled_job('interval', minutes=1)
+def do_request_to_server():
+    requests.get(URL)
+    
+    
+sched_remind.start()
+sched_request.start()
